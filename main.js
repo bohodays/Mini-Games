81bda7416538eee419564d5a023bc8ffe0fad287
bohodays <bohodays0316@gmail.com>
2022-10-28 22:53:34 +0900

    update

---
+++ main.js
@@ -0,0 +1,10 @@
+const firstCards = document.querySelector('.first');
+firstCards.addEventListener('click', () => {
+  window.location.href = 'Rock-Paper-Scissors/index.html';
+})
+
+const secondCards = document.querySelector('.second');
+secondCards.addEventListener('click', () => {
+  window.location.href = '숫자야구/index.html';
+})
+
---
+++ 숫자야구/src/game/playGame.js
@@ -0,0 +1,63 @@
+import getUserInput from '../input/getUserInput.js';
+import printGameResult from './printGameResult.js';
+import getComputerInput from '../input/getComputerInput.js';
+
+
+
+function createGamePage() {
+  const main = document.querySelector('main');
+  const gamePage = document.createElement('div');
+  gamePage.setAttribute('class', 'game-page');
+  gamePage.innerHTML = `
+          <h2 class="game-page__title">
+            ⚾ 숫자 야구
+          </h2>
+          <div class="inputs">
+            <input type="text" class="firstNumber" maxlength='1' placeholder="첫번째 숫자">
+            <input type="text" class="secondNumber" maxlength='1' placeholder="두번째 숫자">
+            <input type="text" class="thirdNumber" maxlength='1' placeholder="세번째 숫자">
+          </div>
+          <div class="bord">
+            <h4 class="bord__title">기록</h4>
+            <div class="bord__records">
+            </div>
+          </div>
+          <button type="submit" class="check__button">
+            확인
+          </button>
+          `;
+          main.append(gamePage);
+};
+
+
+function resetScreen() {
+  const firstInput = document.querySelector('.firstNumber');
+  const secondInput = document.querySelector('.secondNumber');
+  const thirdInput = document.querySelector('.thirdNumber');
+
+  firstInput.value = '';
+  secondInput.value = '';
+  thirdInput.value = '';
+};
+
+
+export default function playGame(computerInputNumbers, play) {
+  const playBtn = document.querySelector('.play__button');
+  const introPage = document.querySelector('.intro');
+
+  playBtn.addEventListener('click', () => {
+    introPage.remove();
+    createGamePage();
+
+    const checkButton = document.querySelector('.check__button');
+  
+    checkButton.addEventListener('click', () => {
+      const userInputNumbers = getUserInput();
+      // by 한별이형
+      if (userInputNumbers) {
+        printGameResult(play(computerInputNumbers, userInputNumbers));
+        resetScreen();
+      }
+    });
+  });
+};
