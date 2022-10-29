1d894fc986ca54b1d025d7cd58f716fb3b0ba786
bohodays <bohodays0316@gmail.com>
2022-10-30 01:25:18 +0900

    edit: CSS, JS

--- main.js
+++ main.js
@@ -8,3 +8,18 @@ secondCards.addEventListener('click', () => {
   window.location.href = '숫자야구/index.html';
 })
 
+const introPage = document.querySelector('.intro');
+const cards = document.querySelectorAll('.cards');
+introPage.addEventListener('click', () => {
+  introPage.style.display = 'none';
+  cards.forEach((card) => {
+    card.style.display = 'block';
+  })
+})
+
+document.addEventListener('keydown', () => {
+  introPage.style.display = 'none';
+  cards.forEach((card) => {
+    card.style.display = 'block';
+  })
+})
